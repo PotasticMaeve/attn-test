8db65c58bf95250a9c5665ba134df1dcff9ba7f6
Claudia Felicia <claudia.ak47@gmail.com>
2022-06-24 14:37:03 +0700

    complete unit test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,15 @@ import Searchbar from "./Searchbar"
 const Navbar = () => {
     return (
         <div className="nav_wrap">
-            <Searchbar />
-            <ul className="menu_list">
-                <i className="ri-home-4-fill"></i>
-                <i className="ri-notification-2-fill"></i>
-                <i className="ri-account-circle-fill"></i>
-            </ul>
+            <h2 className="title">Media App</h2>
+            <div className="right_menu">
+                <Searchbar />
+                <ul className="menu_list" data-testid="menu_list">
+                    <i className="ri-home-4-fill"></i>
+                    <i className="ri-notification-2-fill"></i>
+                    <i className="ri-account-circle-fill"></i>
+                </ul>
+            </div>
         </div>
     )
 }
